cca963f5f5bbe570ec8f7b2611978f444eae6373
Abdul Zindani <azindani101@gmail.com>
2023-08-07 23:50:35 -0600

    remove extra fields

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from .models import Todo
 
 class TodoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'label', 'category', 'description',
-                    'start_date', 'end_date',
-                    'archived', 'completed')
+    list_display = ('title', # 'label', 'category', 
+                    'description',
+                    # 'start_date', 'end_date', 'archived',
+                    'completed')
 
 # Register your models here.
 
--- backend/todo/models.py
+++ backend/todo/models.py
@@ -5,12 +5,12 @@ from django.db import models
 class Todo(models.Model):
     id = models.BigAutoField(auto_created=True, primary_key=True)
     title = models.CharField(max_length=120)
-    label = models.CharField(max_length=120)
+    # label = models.CharField(max_length=120)
     description = models.TextField(blank=True)
-    category = models.CharField(max_length=120)
-    start_date = models.DateField(null=True, blank=True)
-    end_date = models.DateField(null=True, blank=True)
-    archived = models.BooleanField(default=False)
+    # category = models.CharField(max_length=120)
+    # start_date = models.DateField(null=True, blank=True)
+    # end_date = models.DateField(null=True, blank=True)
+    # archived = models.BooleanField(default=False)
     completed = models.BooleanField(default=False)
 
     def _str_(self):
